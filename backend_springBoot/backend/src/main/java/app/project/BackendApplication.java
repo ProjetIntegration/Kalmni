057c8db51ca01bbb4a8db5cbb3d20c92d91c7604
Akram Zaabi <akramzaabi7@gmail.com>
2024-04-09 16:55:11 +0100

    back-end-notifications-entity

--- backend_springBoot/backend/src/main/java/app/project/BackendApplication.java
+++ backend_springBoot/backend/src/main/java/app/project/BackendApplication.java
@@ -1,13 +1,15 @@
 package app.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
-
+public class BackendApplication  {
+	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
