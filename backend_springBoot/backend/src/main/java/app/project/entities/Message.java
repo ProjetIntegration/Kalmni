236fc1f492bc50f4a897291cdc1250f07eb14003
Akram Zaabi <akramzaabi7@gmail.com>
2024-04-09 16:54:33 +0100

    back-end-message-entity

---
+++ backend_springBoot/backend/src/main/java/app/project/entities/Message.java
@@ -0,0 +1,38 @@
+package app.project.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Message")
+public class Message {
+
+	
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	  @Column(name="id")
+	  private int MsgId ; 
+	  
+	  @Column(name="message_txt")
+	  private String message; 
+	  
+	  @Column(name="sender_id")
+	  private int sender_id; 
+	  
+	  @Column(name="receiver_id")
+	  private int receiver_id;
+	  
+	  
+	  @ManyToOne
+	  @JoinColumn(name="fk_PersonneId")
+	  private Personne person_receiver; 
+	  
+	
+	
+}
