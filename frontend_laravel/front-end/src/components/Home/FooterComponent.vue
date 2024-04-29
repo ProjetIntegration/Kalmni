65eb47a90c83e0e4da444741f3757caf439039fd
wessim <wessimsaidani56@gmail.com>
2024-04-29 13:34:18 +0100

    add footer

---
+++ frontend_laravel/front-end/src/components/Home/FooterComponent.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <div
+      class="relative isolate overflow-hidden bg-amber-500 py-24 m-20 border rounded-2xl"
+    >
+      <img
+        src="../../assets/work.jpg"
+        alt=""
+        class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+      <div
+        class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
+      </div>
+      <div
+        class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
+      </div>
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+          <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Boostez Votre activité !
+          </h2>
+          <p class="mt-6 text-lg leading-8 text-white">
+            Créez votre compte gratuitement dès maintenant et Gagnez en
+            visibilité et en notoriété grâce à notre communauté de
+            d'utilisateurs à la recherche de services à domicile et de
+            prestataires qualifiés.
+          </p>
+          <button
+            type="submit"
+            class="text-black mt-5 transition ease-in-out delay-150 font-bold block w-full-xl p-4 pl-10 pr-10 text-center border border-gray-300 rounded-lg bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 text-sm dark:bg-gray-300 dark:hover:bg-gray-400 dark:focus:ring-gray-400 hover:-translate-y-1 hover:scale-110 duration-300 ..."
+          >
+            Devenir un prestataire
+          </button>
+        </div>
+        <div class="mx-auto mt-5 max-w-2xl lg:mx-0 lg:max-w-none">
+          <dl
+            class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4"
+          >
+            <div class="flex flex-col-reverse">
+              <dt class="text-base leading-7 text-gray-100">
+                Offices worldwide
+              </dt>
+              <dd
+                class="text-2xl font-bold leading-9 tracking-tight text-white"
+              >
+                12
+              </dd>
+            </div>
+            <div class="flex flex-col-reverse">
+              <dt class="text-base leading-7 text-gray-100">
+                Full-time colleagues
+              </dt>
+              <dd
+                class="text-2xl font-bold leading-9 tracking-tight text-white"
+              >
+                300+
+              </dd>
+            </div>
+            <div class="flex flex-col-reverse">
+              <dt class="text-base leading-7 text-gray-100">Hours per week</dt>
+              <dd
+                class="text-2xl font-bold leading-9 tracking-tight text-white"
+              >
+                40
+              </dd>
+            </div>
+            <div class="flex flex-col-reverse">
+              <dt class="text-base leading-7 text-gray-100">Paid time off</dt>
+              <dd
+                class="text-2xl font-bold leading-9 tracking-tight text-white"
+              >
+                Unlimited
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
