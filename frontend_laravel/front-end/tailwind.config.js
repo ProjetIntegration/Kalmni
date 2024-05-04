57ad581d5952baf99ed86a8a0cfe10f436cb85fd
wessim <wessimsaidani56@gmail.com>
2024-05-04 22:28:48 +0100

    connfiguration animation

--- frontend_laravel/front-end/tailwind.config.js
+++ frontend_laravel/front-end/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
