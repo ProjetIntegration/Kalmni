4294661e10a325a7228017bb83aa34f87640449f
wessim <wessimsaidani56@gmail.com>
2024-05-05 09:42:43 +0100

    dashbord admin

---
+++ frontend_laravel/front-end/src/components/DashbordAdmin/sidBar.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <nav
+      class="bg-white shadow-lg h-screen fixed top-0 left-0 min-w-[250px] py-6 px-4 font-[sans-serif] overflow-auto"
+    >
+      <a href="javascript:void(0)"
+        ><img src="../../assets/logo2.png" alt="logo" class="w-[160px]" />
+      </a>
+      <ul class="mt-6">
+        <li>
+          <a
+            href="javascript:void(0)"
+            class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="w-[18px] h-[18px] mr-4"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M197.332 170.668h-160C16.746 170.668 0 153.922 0 133.332v-96C0 16.746 16.746 0 37.332 0h160c20.59 0 37.336 16.746 37.336 37.332v96c0 20.59-16.746 37.336-37.336 37.336zM37.332 32A5.336 5.336 0 0 0 32 37.332v96a5.337 5.337 0 0 0 5.332 5.336h160a5.338 5.338 0 0 0 5.336-5.336v-96A5.337 5.337 0 0 0 197.332 32zm160 480h-160C16.746 512 0 495.254 0 474.668v-224c0-20.59 16.746-37.336 37.332-37.336h160c20.59 0 37.336 16.746 37.336 37.336v224c0 20.586-16.746 37.332-37.336 37.332zm-160-266.668A5.337 5.337 0 0 0 32 250.668v224A5.336 5.336 0 0 0 37.332 480h160a5.337 5.337 0 0 0 5.336-5.332v-224a5.338 5.338 0 0 0-5.336-5.336zM474.668 512h-160c-20.59 0-37.336-16.746-37.336-37.332v-96c0-20.59 16.746-37.336 37.336-37.336h160c20.586 0 37.332 16.746 37.332 37.336v96C512 495.254 495.254 512 474.668 512zm-160-138.668a5.338 5.338 0 0 0-5.336 5.336v96a5.337 5.337 0 0 0 5.336 5.332h160a5.336 5.336 0 0 0 5.332-5.332v-96a5.337 5.337 0 0 0-5.332-5.336zm160-74.664h-160c-20.59 0-37.336-16.746-37.336-37.336v-224C277.332 16.746 294.078 0 314.668 0h160C495.254 0 512 16.746 512 37.332v224c0 20.59-16.746 37.336-37.332 37.336zM314.668 32a5.337 5.337 0 0 0-5.336 5.332v224a5.338 5.338 0 0 0 5.336 5.336h160a5.337 5.337 0 0 0 5.332-5.336v-224A5.336 5.336 0 0 0 474.668 32zm0 0"
+                data-original="#000000"
+              />
+            </svg>
+            <span>Dashboard</span>
+          </a>
+        </li>
+      </ul>
+      <div class="mt-6">
+        <h6 class="text-amber-600 text-sm font-bold px-4">Information</h6>
+        <ul class="mt-3">
+          <li>
+            <a
+              href="javascript:void(0)"
+              class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="w-[18px] h-[18px] mr-4"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  d="M437.02 74.98C388.668 26.63 324.379 0 256 0S123.332 26.629 74.98 74.98C26.63 123.332 0 187.621 0 256s26.629 132.668 74.98 181.02C123.332 485.37 187.621 512 256 512s132.668-26.629 181.02-74.98C485.37 388.668 512 324.379 512 256s-26.629-132.668-74.98-181.02zM111.105 429.297c8.454-72.735 70.989-128.89 144.895-128.89 38.96 0 75.598 15.179 103.156 42.734 23.281 23.285 37.965 53.687 41.742 86.152C361.641 462.172 311.094 482 256 482s-105.637-19.824-144.895-52.703zM256 269.507c-42.871 0-77.754-34.882-77.754-77.753C178.246 148.879 213.13 114 256 114s77.754 34.879 77.754 77.754c0 42.871-34.883 77.754-77.754 77.754zm170.719 134.427a175.9 175.9 0 0 0-46.352-82.004c-18.437-18.438-40.25-32.27-64.039-40.938 28.598-19.394 47.426-52.16 47.426-89.238C363.754 132.34 315.414 84 256 84s-107.754 48.34-107.754 107.754c0 37.098 18.844 69.875 47.465 89.266-21.887 7.976-42.14 20.308-59.566 36.542-25.235 23.5-42.758 53.465-50.883 86.348C50.852 364.242 30 312.512 30 256 30 131.383 131.383 30 256 30s226 101.383 226 226c0 56.523-20.86 108.266-55.281 147.934zm0 0"
+                  data-original="#000000"
+                />
+              </svg>
+              <span>Audience</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="javascript:void(0)"
+              class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+            >
+              <svg
+                class="h-5 w-5 text-gray-500"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <circle cx="12" cy="7" r="4" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+              </svg>
+              <span class="ml-3">Prestataire</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="javascript:void(0)"
+              class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+            >
+              <svg
+                class="h-5 w-5 text-gray-500"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <circle cx="12" cy="7" r="4" />
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+              </svg>
+              <span class="ml-3">client</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="javascript:void(0)"
+              class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+            >
+              <svg
+                class="h-5 w-5 text-gray-500"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+                <circle cx="8.5" cy="7" r="4" />
+                <line x1="20" y1="8" x2="20" y2="14" />
+                <line x1="23" y1="11" x2="17" y2="11" />
+              </svg>
+              <span class="ml-3">demande</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="mt-6">
+        <h6 class="text-amber-600 text-sm font-bold px-4">Actions</h6>
+        <ul class="mt-3">
+          <li>
+            <a
+              href="javascript:void(0)"
+              class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="w-[18px] h-[18px] mr-4"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  d="M437.02 74.98C388.668 26.63 324.379 0 256 0S123.332 26.629 74.98 74.98C26.63 123.332 0 187.621 0 256s26.629 132.668 74.98 181.02C123.332 485.37 187.621 512 256 512s132.668-26.629 181.02-74.98C485.37 388.668 512 324.379 512 256s-26.629-132.668-74.98-181.02zM111.105 429.297c8.454-72.735 70.989-128.89 144.895-128.89 38.96 0 75.598 15.179 103.156 42.734 23.281 23.285 37.965 53.687 41.742 86.152C361.641 462.172 311.094 482 256 482s-105.637-19.824-144.895-52.703zM256 269.507c-42.871 0-77.754-34.882-77.754-77.753C178.246 148.879 213.13 114 256 114s77.754 34.879 77.754 77.754c0 42.871-34.883 77.754-77.754 77.754zm170.719 134.427a175.9 175.9 0 0 0-46.352-82.004c-18.437-18.438-40.25-32.27-64.039-40.938 28.598-19.394 47.426-52.16 47.426-89.238C363.754 132.34 315.414 84 256 84s-107.754 48.34-107.754 107.754c0 37.098 18.844 69.875 47.465 89.266-21.887 7.976-42.14 20.308-59.566 36.542-25.235 23.5-42.758 53.465-50.883 86.348C50.852 364.242 30 312.512 30 256 30 131.383 131.383 30 256 30s226 101.383 226 226c0 56.523-20.86 108.266-55.281 147.934zm0 0"
+                  data-original="#000000"
+                />
+              </svg>
+              <span>Profile</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="javascript:void(0)"
+              class="text-black hover:text-amber-600 text-sm flex items-center hover:bg-amber-50 rounded px-4 py-3 transition-all"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                class="w-[18px] h-[18px] mr-4"
+                viewBox="0 0 6.35 6.35"
+              >
+                <path
+                  d="M3.172.53a.265.266 0 0 0-.262.268v2.127a.265.266 0 0 0 .53 0V.798A.265.266 0 0 0 3.172.53zm1.544.532a.265.266 0 0 0-.026 0 .265.266 0 0 0-.147.47c.459.391.749.973.749 1.626 0 1.18-.944 2.131-2.116 2.131A2.12 2.12 0 0 1 1.06 3.16c0-.65.286-1.228.74-1.62a.265.266 0 1 0-.344-.404A2.667 2.667 0 0 0 .53 3.158a2.66 2.66 0 0 0 2.647 2.663 2.657 2.657 0 0 0 2.645-2.663c0-.812-.363-1.542-.936-2.03a.265.266 0 0 0-.17-.066z"
+                  data-original="#000000"
+                />
+              </svg>
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ frontend_laravel/front-end/src/views/DashbordView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Sidbar></Sidbar>
+    <client></client>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import prestataire from "../components/DashbordAdmin/PrestataireAdmin.vue";
+import client from "../components/DashbordAdmin/ClientAdmin.vue";
+import Sidbar from "../components/DashbordAdmin/sidBar.vue";
+
+export default {
+  name: "DashbordView",
+
+  components: {
+    Sidbar,
+    // prestataire,
+    client,
+  },
+};
+</script>
