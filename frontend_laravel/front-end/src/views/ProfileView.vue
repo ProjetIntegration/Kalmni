0bedc984d3280b06c6ba7dc95599a3256230deb0
wessim <wessimsaidani56@gmail.com>
2024-05-05 09:41:55 +0100

    profil prestataire

---
+++ frontend_laravel/front-end/src/views/ProfileView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <Acceuil></Acceuil>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Acceuil from "../components/Prestataire/AcceuilComponent.vue";
+import Navbar from "../components/NavbarComponent.vue";
+
+export default {
+  name: "ProfileView",
+
+  components: {
+    Navbar,
+    Acceuil,
+  },
+};
+</script>
