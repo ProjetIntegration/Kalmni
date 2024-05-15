d21f145db841c9d21908b6bb0da8d14124d502c2
Ihebsessi <ihebsessi29@gmail.com>
2024-05-15 20:38:43 +0100

    add service

--- frontend_laravel/front-end/src/service/Service.js
+++ frontend_laravel/front-end/src/service/Service.js
@@ -10,11 +10,29 @@
     
         Service_recherche(Service)
          {
-
             let data = new FormData();
             data.append("nom_service", user.nom_service);
             data.append("location", user.location);
-
-
+         },
+         
+         AddService(data){
+          let payload = new FormData();
+          payload.append("nom", data.nom);
+          payload.append("description", data.description);
+          payload.append("addresse", data.addresse);
+          payload.append("photo", data.photo);
+          payload.append("photo_certif", data.photo_certif);
+          payload.append("user_id", data.user_id);
+          payload.append("category_id", data.category_id);
+          payload.append("tab_schedules", JSON.stringify(data.tab_schedules));
+          const config = {
+              Headers: {
+                  "content-type": "multipart/form-data",
+              },
+          };
+          console.log(data);
+          return ApiClient.post("/service/AddService", payload, config);
          }
+
+
  };
