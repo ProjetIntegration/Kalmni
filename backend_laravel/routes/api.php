bb6c8d15de5b1f1dd12caddee48d30e3896ff56a
Ihebsessi <ihebsessi29@gmail.com>
2024-05-15 20:43:02 +0100

    add service

--- backend_laravel/routes/api.php
+++ backend_laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\auth\SignUpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,4 +23,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(["prefix"=>"/auth"],function(){
     Route::post('/signup',[SignUpController::class,'Signup']);
     
+});
+
+
+Route::group(["prefix"=>"/service"],function(){
+    Route::post('/AddService',[ServiceController::class,'addServices']);
 });
