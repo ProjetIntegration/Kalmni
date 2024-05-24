b7f0bf158c83ed248cc28372aa44b15c4d0b1809
wessim <wessimsaidani56@gmail.com>
2024-05-25 00:47:40 +0100

    correct navbar

--- frontend_spring/front-end/src/components/NavbarComponent.vue
+++ frontend_spring/front-end/src/components/NavbarComponent.vue
@@ -14,16 +14,18 @@
           </div>
         </div>
 
-        <!--<div v-if="user" class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse pr-10">
+        <div v-if="user" class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse pr-10">
           <div class="flex items-center gap-4">
-            <img class="w-10 h-10 rounded-full" :src="user.photo" alt="">
+            <img class="w-10 h-10 rounded-full" :src="'http://localhost:8000' + user.photo" alt="">
             <div class="font-medium dark:text-dark">
               <div>{{ user.nom + " " + user.prenom }}</div>
               <button @click="signOut()" class="text-sm text-gray-500 dark:text-gray-400">sign out</button>
             </div>
+            
+
           </div>
-        </div>-->
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse" >
+        </div>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse" v-else>
           <button type="button"
             class="transition ease-in-out delay-150 text-black font-bold mr-3 hover:-translate-y-1 hover:scale-110 duration-300 ...">
             <router-link to="/SigneIn">sign in</router-link>
@@ -90,10 +92,11 @@ import router from "@/router";
 export default {
   computed: {
     user() {
-    /*  let x = localStorage.getItem("user")
+
+      let x = localStorage.getItem("user")
         ? JSON.parse(localStorage.getItem("user"))
-        : "";*/
-      return true;
+        : "";
+      return x;
     },
   },
   data() {
