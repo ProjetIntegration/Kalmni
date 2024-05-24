430dd7e8f709b047517bde98f8453e685d720b20
wessim <wessimsaidani56@gmail.com>
2024-05-24 18:53:47 +0100

    get service back

--- backend_springBoot/backend/src/main/java/app/project/controllers/ServicesController.java
+++ backend_springBoot/backend/src/main/java/app/project/controllers/ServicesController.java
@@ -1,7 +1,10 @@
 package app.project.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +25,32 @@ public class ServicesController {
 		
 	    return servicesimp.saveService(s);
 	}
+	@GetMapping("/Recherche")
+	public List<Services> rechercheService(@RequestParam("nom") String nom, @RequestParam("adresse") String adresse) {
+	    List<Services> services;
+
+        if (adresse == null && nom != null) {
+            services = servicesimp.findByNomContaining(nom);
+        } else if (adresse != null && nom == null) {
+            services = servicesimp.findByAddresseContaining(adresse);
+        } else {
+            services = servicesimp.FindServicesByLocation(nom, adresse);
+
+        }
+
+        return services;
+	}
+	@GetMapping("/getAllServices")
+	public List<Services> getAllServices() {
+	    return servicesimp.findAllServices();
+	}
+	@GetMapping("/getServicesByNom")
+	public List<Services> getServicesByNom(@RequestParam("nom") String nom) {
+	    return servicesimp.findByNomContaining(nom);
+	}
+
+
+	
 	
 
 	
